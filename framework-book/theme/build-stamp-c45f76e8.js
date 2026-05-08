3c29d4daaf78328389f1bd3e6586d81ddc441325
Wolfgang Grieskamp <wg@aptoslabs.com>
2026-05-08 17:58:02 +0000

    deploy framework book from aptos-core bdeca77d3e (2026-05-08 17:58 UTC)

--- framework-book/theme/build-stamp-c45f76e8.js
+++ framework-book/theme/build-stamp-c45f76e8.js
@@ -1,6 +1,6 @@
 // Appends a build-stamp footer to every page.
 //
-// The literal "2026-05-08 16:44 UTC (debug build, aptos-core 7f352ca6a2)" is the placeholder that `deploy.sh` substitutes
+// The literal "2026-05-08 17:58 UTC (aptos-core bdeca77d3e)" is the placeholder that `deploy.sh` substitutes
 // with the deploy timestamp + source commit hash. Local mdbook builds leave
 // it untouched, signalling that the page wasn't deployed.
 (function () {
@@ -9,7 +9,7 @@
     if (!main || main.querySelector('.build-stamp')) return;
     var p = document.createElement('p');
     p.className = 'build-stamp';
-    p.innerHTML = '<em>Build: 2026-05-08 16:44 UTC (debug build, aptos-core 7f352ca6a2)</em>';
+    p.innerHTML = '<em>Build: 2026-05-08 17:58 UTC (aptos-core bdeca77d3e)</em>';
     main.appendChild(p);
   }
   if (document.readyState === 'loading') {
