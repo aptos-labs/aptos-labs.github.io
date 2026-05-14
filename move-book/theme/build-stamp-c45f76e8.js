7e636ec2bb944355d11e955c1a9cb05334c17cf1
Wolfgang Grieskamp <wg@aptoslabs.com>
2026-05-14 16:57:45 +0000

    deploy move book from aptos-core 135c42e851 (2026-05-14 16:57 UTC)

--- move-book/theme/build-stamp-c45f76e8.js
+++ move-book/theme/build-stamp-c45f76e8.js
@@ -1,6 +1,6 @@
 // Appends a build-stamp footer to every page.
 //
-// The literal "2026-05-08 17:59 UTC (aptos-core bdeca77d3e)" is the placeholder that `deploy.sh` substitutes
+// The literal "2026-05-14 16:57 UTC (aptos-core 135c42e851)" is the placeholder that `deploy.sh` substitutes
 // with the deploy timestamp + source commit hash. Local mdbook builds leave
 // it untouched, signalling that the page wasn't deployed.
 (function () {
@@ -9,7 +9,7 @@
     if (!main || main.querySelector('.build-stamp')) return;
     var p = document.createElement('p');
     p.className = 'build-stamp';
-    p.innerHTML = '<em>Build: 2026-05-08 17:59 UTC (aptos-core bdeca77d3e)</em>';
+    p.innerHTML = '<em>Build: 2026-05-14 16:57 UTC (aptos-core 135c42e851)</em>';
     main.appendChild(p);
   }
   if (document.readyState === 'loading') {
