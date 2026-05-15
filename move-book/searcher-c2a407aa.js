ee7df76e85c5bb685903cd624a620ccbf1990c75
Teng Zhang <teng@aptoslabs.com>
2026-05-14 19:58:28 -0700

    deploy move book from aptos-core 14dfd48dab (2026-05-15 02:58 UTC)

--- move-book/searcher-c2a407aa.js
+++ move-book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-41124df0.js',
+                path_to_root + 'searchindex-a27d1de9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
